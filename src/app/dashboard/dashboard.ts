648558c271a7e64806b32693bee9e8c003721828
drowland1159 <drowland1159@sdsu.edu>
2026-05-10 18:49:23 -0700

    Backend code to display produce listings

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { produce_listings } from '../model/listing.model';
 import { SupabaseService } from '../services/supabase';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
@@ -14,6 +15,9 @@ export class Dashboard implements OnInit {
   listings: produce_listings[] = [];
   isLoading = true;
   message = '';
+  searchQuery = '';
+  minQuantity : number | null = null;
+  maxQuantity : number | null = null;
 
   constructor(private supabaseService: SupabaseService, private cdr: ChangeDetectorRef) {}
 
@@ -32,7 +36,8 @@ export class Dashboard implements OnInit {
         console.error('Error occurred while fetching list:', error);
         this.message = 'Can\'t load listings. Please retry.';
       } else {
-        this.listings = listings ?? []
+        this.listings = listings ?? [];
+        this.filteredListings = this.listings;
       }
     } catch (err) {
       console.error(err);
@@ -43,8 +48,30 @@ export class Dashboard implements OnInit {
     this.cdr.detectChanges();
   }
 
-scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
+  filteredListings: produce_listings[] = [];
+
+  applyFilters() {
+    let results = this.listings;
+
+    if(this.searchQuery.trim()) {
+      const query = this.searchQuery.toLocaleLowerCase();
+      results = results.filter(listing => listing.title?.toLowerCase().includes(query))
+    }
+
+    if(this.minQuantity !== null && this.minQuantity > 0) {
+      results = results.filter(listing => listing.quantity >= this.minQuantity!);
+    }
+
+    if(this.maxQuantity !== null && this.maxQuantity > 0) {
+      results = results.filter(listing => listing.quantity <= this.maxQuantity!);
+    }
+
+    this.filteredListings = results;
+    this.cdr.detectChanges();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
